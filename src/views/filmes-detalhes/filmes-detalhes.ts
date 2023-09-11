8082717eaafe48dee7a26e41e6bbae5a1e979ce9
Camile Coelho <camilecoelho23@gmail.com>
2023-09-11 01:43:04 -0300

    commit.

--- src/views/filmes-detalhes/filmes-detalhes.ts
+++ src/views/filmes-detalhes/filmes-detalhes.ts
@@ -1,25 +1,222 @@
-import { API_KEY } from "../../../secrets";
 import "./filmes-detalhes.css";
+import { FilmeService } from '../../../services/filme.service'; // Substitua pelo caminho correto para o seu FilmeService
 
 export class DetalhesFilme {
-  constructor() {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=pt-BR",
-      this.obterHeaderAutorizacao()
-    )
-      .then((res) => res.json())
-      .then((obj) => console.log(obj));
-  }
+  private filmeService: FilmeService;
 
-  private obterHeaderAutorizacao() {
-    return {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    };
+  constructor() {
+    this.filmeService = new FilmeService();
+
+    const idDoFilme = '335977'; // Substitua pelo ID do filme que você deseja buscar
+
+    this.filmeService.selecionarFilmePorId(idDoFilme)
+      .then((detalhesFilme) => {
+        // Preencha os elementos HTML com os dados do filme
+    const tituloElement = document.querySelector('.titulo-filme');
+    const votosElement = document.querySelector('.votos-filme');
+    const sinopseElement = document.querySelector('.sinopse-filme');
+    const lancamentoElement = document.getElementById('data-lancamento-filme');
+    const posterElement = document.querySelector('.poster-filme')
+
+    if (tituloElement) {
+      tituloElement.textContent = detalhesFilme.titulo;
+    }
+
+    if (votosElement) {
+      votosElement.textContent = `${detalhesFilme.contagemVotos} Votos`;
+    }
+
+    if (sinopseElement) {
+      sinopseElement.textContent = detalhesFilme.sinopse;
+    }
+
+    if (lancamentoElement) {
+      lancamentoElement.textContent = detalhesFilme.dataLancamento;
+    }
+
+    if (posterElement) {
+      posterElement.setAttribute('src', detalhesFilme.urlPoster);
+    }
+
+    console.log(detalhesFilme);
+      })
+      .catch((error) => {
+        // Lide com erros, se necessário
+        console.error(error);
+      });
+      
   }
 }
 
 window.addEventListener("load", () => new DetalhesFilme());
+
+
+
+
+// import { FilmeService } from "../../../services/filme.service"
+// import { LocalStorageService } from "../../../services/local-storage.service";
+// import { FilmeFavorito } from "../../../models/filme-favorito";
+// import { CreditosFilme } from "../../../models/creditos-filme";
+// import { DetalhesFilmes } from "../../../models/detalhes-filme";
+// import "./filmes-detalhes.css";
+
+// // export class DetalhesFilme {
+// //   constructor() {
+// //     var filmeService = new FilmeService();
+
+// //     var teste = filmeService.selecionarFilmePorId("335977")
+// //     console.log(teste)
+// //   }
+
+// // }
+
+// // window.addEventListener("load", () => new DetalhesFilme());
+
+
+// const params = new URLSearchParams(window.location.search);
+
+// const id = params.get("id") as string;
+
+// const filmeService = new FilmeService();
+// const localStorageService = new LocalStorageService();
+
+// const adicionarFavoritos = document.getElementById('adicionar-favorito');
+
+// window.addEventListener('load', () => {
+
+//   filmeService.selecionarFilmePorId(id).then(filme => {
+//     document.title = filme.titulo + ' - Gerenciador de Cinema';
+
+//     const img = document.getElementById('poster-filme') as HTMLImageElement;
+//     img.src = filme.urlPoster;
+    
+//     const titulo = document.getElementById('titulo-filme');
+//     titulo!.textContent = filme.titulo;
+
+//     const dataLancamento = document.getElementById('data-lancamento');
+//     dataLancamento!.textContent = filme.dataLancamento;
+
+//     const secaoGeneros = document.getElementById('generos-filme');
+
+//     filme.generos.forEach(genero => {
+//       const elementoGenero = document.createElement('button');
+//       elementoGenero.className = 'genero-filme';
+//       elementoGenero.textContent = genero;
+
+//       secaoGeneros?.appendChild(elementoGenero);
+//     });
+
+//     const sinopse = document.getElementById('sinopse-filme');
+//     sinopse!.textContent = filme.sinopse;
+//   })
+
+//   filmeService.selecionarTrailersFilmePorId(id).then(trailers => {
+//     const video = document.getElementById('video-filme') as HTMLIFrameElement;
+//     video.src = trailers[0]?.sourceUrl ?? '' ;
+//   });
+
+//   filmeService.selecionarCreditosFilmePorId(id).then(creditos => {
+//     selecionarDiretor(creditos);
+//     selecionarEscritores(creditos);
+//     selecionarAtores(creditos);
+//   });
+
+//   carregarStatusFavorito();
+// });
+
+// adicionarFavoritos?.addEventListener('click', (e) => {
+//   e.preventDefault();
+
+//   const favoritoEncontrado = localStorageService.selecionarPorId(id);
+  
+//   const icone = adicionarFavoritos!.children[0] as HTMLElement;
+
+//   if (!favoritoEncontrado)
+//   {
+//     const novoFavorito: FilmeFavorito = { id: id };
+//     localStorageService.inserir(novoFavorito);
+
+//     icone.textContent = 'heart_minus';
+//     icone.title = 'Remover favorito';
+//     return;
+//   }
+
+//   localStorageService.excluir(id);
+
+//   adicionarFavoritos.children[0].textContent = 'heart_plus';
+//   icone.title = 'Adicionar favorito';
+// });
+
+// function selecionarDiretor(creditos: CreditosFilme[]) {
+//   const diretor = creditos.find(c => c.departamento == 'Directing');
+
+//   if (!diretor)
+//     return;
+
+//   const secaoDiretor = document.getElementById('diretor-filme');
+
+//   const labelDiretor = document.createElement('p');
+//   labelDiretor.textContent =  diretor.nome;
+
+//   secaoDiretor?.appendChild(labelDiretor);
+// }
+
+// function selecionarEscritores(creditos: CreditosFilme[]) {
+//   const escritores = creditos
+//     .filter(c => c.departamento == 'Writing')
+//     .sort((b, a) => b.ordem - a.ordem)
+//     .slice(0, 5);
+
+//   const secaoEscritores = document.getElementById('escritores-filme');
+
+//   escritores.forEach(escritor => {
+//     const novoCredito = document.createElement('p');
+//     novoCredito.textContent =  escritor.nome;
+
+//     const separador = document.createElement('p');
+//     separador.textContent = ' • ';
+
+//     secaoEscritores?.appendChild(novoCredito);
+
+//     if (escritor != escritores[escritores.length - 1])
+//       secaoEscritores?.appendChild(separador);
+//   });
+// }
+
+// function selecionarAtores(creditos: CreditosFilme[]) {
+//   const atores = creditos
+//   .filter(c => c.departamento == 'Acting')
+//   .sort((b, a) => b.ordem - a.ordem)
+//   .slice(0, 5);
+
+//   const secaoAtores = document.getElementById('atores-filme');
+
+//   atores.forEach(ator => {
+//     const novoCredito = document.createElement('p');
+//     novoCredito.textContent =  ator.nome;
+
+//     const separador = document.createElement('p');
+//     separador.textContent = ' • ';
+
+//     secaoAtores?.appendChild(novoCredito);
+
+//     if (ator != atores[atores.length - 1])
+//       secaoAtores?.appendChild(separador);
+//   });
+// }
+
+// function carregarStatusFavorito() {
+//   const favoritoEncontrado = localStorageService.selecionarPorId(id);
+
+//   const icone = adicionarFavoritos!.children[0] as HTMLElement;
+
+//   if (favoritoEncontrado)
+//   {
+//     icone.textContent = 'heart_minus';
+//     icone.title = 'Remover favorito';
+//     return;
+//   }
+
+//   icone.textContent = 'heart_plus';
+//   icone.title = 'Adicionar favorito';
+// }
--- src/views/filmes-listagem/filmes-listagem.ts
+++ src/views/filmes-listagem/filmes-listagem.ts
@@ -1,2 +1,123 @@
 import 'bootstrap'
 import './filmes-listagem.css'
+import { FilmeService } from '../../../services/filme.service';
+import { LocalStorageService } from '../../../services/local-storage.service';
+
+const filmeService = new FilmeService();
+const localStorageService = new LocalStorageService();
+
+window.addEventListener('load', () => {
+  selecionarFilmesFavoritos();
+  selecionarFilmesPorPopularidade();
+  // selecionarFilmesPorAvaliacao();
+});
+
+
+function selecionarFilmesPorPopularidade() {
+  filmeService.selecionarFilmesPopulares()
+    .then(filmes => {
+      const secao = document.getElementById('filmes-por-popularidade');
+
+      const filmesFavoritos = localStorageService.selecionarTodos();
+
+      filmes.forEach(filme => {
+        const poster = document.createElement('div');
+        poster.className = 'poster-filme';
+
+        poster.addEventListener('click', () => {
+          window.location.href = `filmes-detalhes.html?id=${filme.id}`;
+        });
+
+        const img = document.createElement('img');
+        img.src = filme.caminho_poster
+
+        const titulo = document.createElement('p');
+        titulo.textContent = filme.titulo;
+        
+        const filmeEncontrado = filmesFavoritos.find(x => x.id == filme.id.toString())
+        
+        if (filmeEncontrado != undefined)
+          img.className = 'borda-filme-favorito';
+
+        poster?.appendChild(img);
+        poster?.appendChild(titulo);
+        poster!.title = filme.titulo;
+
+        secao?.appendChild(poster);
+      });
+    });
+}
+
+function selecionarFilmesPorAvaliacao() {
+  filmeService.selecionarFilmesPorAvaliacao()
+    .then(filmes => {
+      const secao = document.getElementById('filmes-por-avaliacao');
+
+      const filmesFavoritos = localStorageService.selecionarTodos();
+
+      filmes.forEach(filme => {
+        const poster = document.createElement('div');
+        poster.className = 'poster-filme';
+
+        poster.addEventListener('click', () => {
+          window.location.href = `filmes-detalhes.html?id=${filme.id}`;
+        });
+
+        const img = document.createElement('img');
+        img.src = filme.caminho_poster
+
+        const titulo = document.createElement('p');
+        titulo.textContent = filme.titulo;
+        
+        const filmeEncontrado = filmesFavoritos.find(x => x.id == filme.id.toString())
+        
+        if (filmeEncontrado != undefined)
+          img.className = 'borda-filme-favorito';
+
+        poster?.appendChild(img);
+        poster?.appendChild(titulo);
+        poster!.title = filme.titulo;
+
+        secao?.appendChild(poster);
+      });
+    });
+}
+
+function selecionarFilmesFavoritos() {
+  const filmesFavoritos = localStorageService.selecionarTodos();
+
+  if (filmesFavoritos.length < 1) return;
+
+  const lblHeaderFavoritos = document.getElementById('header-favoritos');
+  const lblAviso = document.getElementById('aviso-sem-favoritos');
+
+  lblHeaderFavoritos!.style.display = 'block';
+  lblAviso!.style.display = 'block';
+
+  const secao = document.getElementById('filmes-favoritos');
+
+  filmesFavoritos.slice(0, 9).forEach(filme => {
+    const listagem = filmeService.selecionarFilmeFavorito(filme.id);
+      
+    listagem.then(filmeFavorito => {
+      const poster = document.createElement('div');
+      poster.className = 'poster-filme';
+  
+      poster.addEventListener('click', () => {
+        window.location.href = `filmes-detalhes.html?id=${filme.id}`;
+      });
+  
+      const img = document.createElement('img');
+      img.src = filmeFavorito.caminho_poster
+  
+      const titulo = document.createElement('p');
+      titulo.textContent = filmeFavorito.titulo;
+      
+      poster?.appendChild(img);
+      poster?.appendChild(titulo);
+      poster!.title = filmeFavorito.titulo;
+  
+      secao?.appendChild(poster);
+    });
+  });
+}
